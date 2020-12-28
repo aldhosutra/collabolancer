45c42d6dd2da640a3a2bf13afe2a553ddccfbc82
Aldo Suhartono Putra <aldhosutra@gmail.com>
2020-12-28 15:43:45 +0700

    fix install script and readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # collabolancer
+
 Collabolancer is a proof-of-concept smart freelancing platform that enables seamless collaboration. Let's make a better world together, not alone. https://collabolancer.com
+
+## Setup Dependencies
+
+Run this script to setup and install all dependencies required to run Collabolancer Node:
+
+```
+curl -o- https://raw.githubusercontent.com/aldhosutra/collabolancer/master/install.sh | bash
+```
+
+## Configure & Run
+
+First, copy template.env as .env, and configure it's value depend on your Node Settings:
+
+```
+SEED_NODE_IP=0.0.0.0
+API_WHITELIST_IP=0.0.0.0
+IS_API_NODE=false
+```
+
+Then, run node using this command:
+
+```
+node index.js | npx bunyan -o short
+```
--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 if [ ! -f ".env" ]; then
   while true; do
-    read -p ".env file are not present, do you want to copy from template.env and use already defined values?" yn
+    read -p ".env file are not present, do you want to copy from template.env and use already defined values to proceed? [y/n] " yn
     case $yn in
-        [Yy]* ) cp template.env .env; break;;
-        [Nn]* ) exit 1;;
+        [Yy]* ) curl -o template.env https://raw.githubusercontent.com/aldhosutra/collabolancer/master/template.env; cp template.env .env; break;;
+        [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
   done
@@ -11,30 +13,33 @@ fi
 
 export $(egrep -v '^#' .env | xargs)
 if [[ -z $TZ || -z $USER_NAME || -z $USER_PASSWORD || -z $DB_NAME || -z $DB_PASSWORD ]]; then
-  echo 'TZ, USER_NAME, USER_PASSWORD, DB_NAME, and DB_PASSWORD,variable are required, please check .env file!'
+  echo 'TZ, USER_NAME, USER_PASSWORD, DB_NAME, and DB_PASSWORD,variable are required, please check .env file or manually set environment variables!'
   exit 1
 fi
 
+echo ""
 echo "#################### Setup User ####################"
+echo ""
 
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 sudo apt-get update
 sudo apt-get -y install nano
 
 if id "$USER_NAME" &>/dev/null; then
-  echo `User $USER_NAME already exist, proceeding to next step!`
+  echo "User $USER_NAME already exist, proceeding to next step!"
 else
-  addgroup --gid $(id -g) $USER_NAME
-  adduser --disabled-password --gecos '' --uid $(id -u) --gid $(id -g) $USER_NAME && usermod -a -G sudo $USER_NAME && echo "$USER_NAME:$USER_PASSWORD" | chpasswd
+  adduser --disabled-password --gecos '' $USER_NAME && usermod -a -G sudo $USER_NAME && echo "$USER_NAME:$USER_PASSWORD" | chpasswd
 fi
 
+echo ""
 echo "#################### Setup Postgres ####################"
+echo ""
 
 which psql
 if [ "$?" -gt "0" ]; then
   sudo apt-get purge -y postgres* 
   sudo apt-get update
-  sudo apt-get install -y lsb-release libtool automake autoconf curl python2-minimal build-essential redis-server wget ca-certificates git language-pack-en
+  sudo apt-get install -y lsb-release libtool automake autoconf python2-minimal build-essential redis-server wget ca-certificates git language-pack-en
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
   sudo apt-get update && apt-get upgrade -y
@@ -43,43 +48,14 @@ if [ "$?" -gt "0" ]; then
   sudo pg_createcluster --locale en_US.UTF-8 --start 10 main
 fi
 
-if psql -t -c '\du' | cut -d \| -f 1 | grep -qw $USER_NAME; then
-  echo `User $USER_NAME already exist on PostgreSQL, proceeding to next step!`
-else
-  sudo -u postgres -i createuser --createdb $USER_NAME
-fi
+sudo -u postgres -i psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$USER_NAME'" | grep -q 1 || sudo -u postgres -i createuser --createdb $USER_NAME
 
-if psql -lqt | cut -d \| -f 1 | grep -qw $DB_NAME; then
-  echo `Database $DB_NAME already exist, proceeding to next step!`
+if sudo -u postgres -i psql -lqt | cut -d \| -f 1 | grep -qw $DB_NAME; then
+  echo "Database $DB_NAME already exist, proceeding to next step!"
 else
   sudo -u postgres -i createdb $DB_NAME --owner $USER_NAME
   sudo -u postgres psql -d $DB_NAME -c "alter user $USER_NAME with password '$DB_PASSWORD';"
 fi
-
-echo "#################### Setup Nodejs via NVM ####################"
-
-su $USER_NAME <<'EOF'
-which nvm
-if [ "$?" -gt "0" ]; then
-  echo "Installing Node as $(whoami), Home: $HOME"
-
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-fi
-
-nvm install 12.15.0
-
-npm install pm2 -g
-npm install --global --production lisk-commander
-EOF
-
-sudo -S ln -sfn "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
-sudo -S ln -sfn "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
-sudo -S ln -sfn "$NVM_DIR/versions/node/$(nvm version)/bin/lisk" "/usr/local/bin/lisk"
-
-echo "#################### Configuring ####################"
 
 if ! grep -Fxq "host all  all    0.0.0.0/0  md5" /etc/postgresql/10/main/pg_hba.conf
 then
@@ -91,7 +67,49 @@ then
   echo "listen_addresses='*'" >> /etc/postgresql/10/main/postgresql.conf
 fi
 
-echo "#################### Run Node ####################"
 sudo /etc/init.d/postgresql restart
-su $USER_NAME
-node index.js | npx bunyan -o short
+
+echo ""
+echo "#################### Setup Nodejs via NVM ####################"
+echo ""
+
+su $USER_NAME <<'EOF'
+echo "Installing Node as $(whoami), Home: $HOME"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install 12.15.0
+
+npm install pm2 -g
+npm install --global --production lisk-commander
+
+echo $USER_PASSWORD | sudo -S ln -sfn "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+echo $USER_PASSWORD | sudo -S ln -sfn "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
+echo $USER_PASSWORD | sudo -S ln -sfn "$NVM_DIR/versions/node/$(nvm version)/bin/lisk" "/usr/local/bin/lisk"
+EOF
+
+while true; do
+  read -p "Setup Dependencies Done, do you want to automatically clone Collabolancer Node on ~/? [y/n] " yn
+  case $yn in
+      [Yy]* ) 
+        sudo -u $USER_NAME -i <<'EOF'
+          cd ~
+          git clone https://github.com/aldhosutra/collabolancer
+          cd collabolancer
+          npm install
+          echo "Collabolancer Node is Installed on $(pwd)"
+          echo "Please Provide .env and run:" 
+          echo ""
+          echo "node index.js | npx bunyan -o short"
+          echo ""
+          echo "Using user '$(whoami)', to start the node!"
+EOF
+        break
+        ;;
+      [Nn]* ) exit 1;;
+      * ) echo "Please answer yes or no.";;
+  esac
+done
